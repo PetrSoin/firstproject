b799339717b3a0e60df70216c3e34487e7356f39
Peter Soin <peter.soin@bk.ru>
2023-10-19 20:42:24 +0300

    add file

---
+++ Untitled-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+    string s1,s2,s;
+    setlocale(LC_ALL, "ru");
+    s1 = "Привет";
+    s2 = "net";
+    s = s1 + ", " + s2 + "!";
+    cout << s;
+  
+}
